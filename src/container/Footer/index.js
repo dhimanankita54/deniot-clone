8629a5f954f549517b74e233fc03ef6f55c317a6
Ankita Dhiman <ankita@bhumiitech.com>
2024-09-30 15:52:40 +0530

    updated footer

--- src/container/Footer/index.js
+++ src/container/Footer/index.js
@@ -4,7 +4,7 @@ import { motion, useScroll } from "framer-motion";
 import { Typography } from "@material-tailwind/react";
 import gsap from "gsap";
 
-function Item({ text, image }) {
+function Item({ text, image, index, length }) {
     const ref = useRef(null);
     const textRef = useRef(null);
     const sectionRef = useRef(null);
@@ -31,19 +31,21 @@ function Item({ text, image }) {
     return (
         <section className="footer-section" ref={sectionRef}>
             <div ref={ref}>
-                <figure className="progress">
-                    <svg id="progress" width="75" height="75" viewBox="0 0 100 100">
-                        <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-                        <motion.circle
-                            cx="50"
-                            cy="50"
-                            r="30"
-                            pathLength="1"
-                            className="indicator"
-                            style={{ pathLength: scrollYProgress }}
-                        />
-                    </svg>
-                </figure>
+                {index !== length - 1 &&
+                    <figure className="progress">
+                        <svg id="progress" width="75" height="75" viewBox="0 0 100 100">
+                            <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
+                            <motion.circle
+                                cx="50"
+                                cy="50"
+                                r="30"
+                                pathLength="1"
+                                className="indicator"
+                                style={{ pathLength: scrollYProgress }}
+                            />
+                        </svg>
+                    </figure>
+                }
                 <div className="flex w-[460px] h-[270px] items-center justify-center" style={{
                     backgroundImage: [`url(${image})`],
                     backgroundRepeat: 'no-repeat',
@@ -63,14 +65,22 @@ function Item({ text, image }) {
 
 export default function Footer() {
 
-    
+    const items = [
+        { text: "Interior Design", image: "https://images.unsplash.com/photo-1463797221720-6b07e6426c24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGludGVyaW9yJTIwZGVzaWdufGVufDB8fDB8fHww" },
+        { text: "Architecture", image: "https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJjaGl0ZWN0dXJlfGVufDB8fDB8fHww" },
+        { text: "Furniture", image: "https://plus.unsplash.com/premium_photo-1684338795288-097525d127f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZ1cm5pdHVyZXxlbnwwfHwwfHx8MA%3D%3D" },
+        { text: "Books", image: "https://images.unsplash.com/photo-1616330682546-2468b2d8dd17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJvb2tzfGVufDB8fDB8fHww" },
+    ];
 
     return (
         <>
-            <Item text="Interior Design" image="https://images.unsplash.com/photo-1463797221720-6b07e6426c24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGludGVyaW9yJTIwZGVzaWdufGVufDB8fDB8fHww" />
-            <Item text="Architecture" image="https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJjaGl0ZWN0dXJlfGVufDB8fDB8fHww" />
-            <Item text="Furniture" image="https://plus.unsplash.com/premium_photo-1684338795288-097525d127f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZ1cm5pdHVyZXxlbnwwfHwwfHx8MA%3D%3D" />
-            <Item text="Books" image="https://images.unsplash.com/photo-1616330682546-2468b2d8dd17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJvb2tzfGVufDB8fDB8fHww" />
+            {items.map((x, index) => (
+                <Item key={index} text={x.text} image={x.image} index={index} length={items.length} />
+            ))}
+
+            <div className="bg-[#8e8c8c] flex items-center justify-center p-4 text-white font-alike w-full h-fit">
+                Created By Ankita Dhiman ❤️
+            </div>
         </>
     );
 }
